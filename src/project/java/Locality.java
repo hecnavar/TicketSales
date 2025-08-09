523f1bb24dec545b7993a0822049949237b9c6a3
hecnavar <hmnm0007@gmail.com>
2025-08-09 00:11:28 -0600

    Feat: Add getters for name and price in Locality

--- src/project/java/Locality.java
+++ src/project/java/Locality.java
@@ -1,4 +1,38 @@
 package project.java;
 
-public class Locality {
+import java.io.Serializable;
+
+public class Locality implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private double price;
+    private int totalCapacity;
+    private int ticketsSold;
+
+    public Locality(String name, double price, int capacity) {
+        this.name = name;
+        this.price = price;
+        this.totalCapacity = capacity;
+        this.ticketsSold = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getAvailable() {
+        return totalCapacity - ticketsSold;
+    }
+
+    public boolean sellTickets(int quantity) {
+        if (quantity > 0 && getAvailable() >= quantity) {
+            this.ticketsSold += quantity;
+            return true;
+        }
+        return false;
+    }
 }
